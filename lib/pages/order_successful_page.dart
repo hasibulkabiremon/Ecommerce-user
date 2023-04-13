8a5bcdfa65bc752a879ceba3186dfe01dd5fd763
hasibulkabiremon <hasibul15-10673@diu.edu.bd>
2023-04-13 11:27:40 +0600

    Animated promopt

--- lib/pages/order_successful_page.dart
+++ lib/pages/order_successful_page.dart
@@ -2,27 +2,169 @@ import 'package:flutter/material.dart';
 
 class OrderSuccessfulPage extends StatelessWidget {
   static const String routeName = '/successful';
+
   const OrderSuccessfulPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
+        iconTheme: const IconThemeData(color: Colors.black),
         backgroundColor: Colors.white,
-        title: const Text('Order Placed',style: TextStyle(color: Colors.black),),
+        title: const Text(
+          'Order Placed',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Icon(
-              Icons.done,
-              size: 150,
-              color: Colors.green,
-            ),
-            const Text('Your order has been placed.')
-          ],
+      body: const Center(
+          child: AnimatedPrompt(
+        title: 'Thank you for your Order!',
+        subtitle: 'You order will be delivered soon. Enjoy!',
+        child: Icon(Icons.check),
+      )),
+    );
+  }
+}
+
+class AnimatedPrompt extends StatefulWidget {
+  final String title;
+  final String subtitle;
+  final Widget child;
+
+  const AnimatedPrompt({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  State<AnimatedPrompt> createState() => _AnimatedPromptState();
+}
+
+class _AnimatedPromptState extends State<AnimatedPrompt>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _iconScaleAnimation;
+  late Animation<double> _containerScaleAnimation;
+  late Animation<Offset> _yAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _yAnimation = Tween<Offset>(
+      begin: const Offset(0, 0),
+      end: const Offset(0, -0.23),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.bounceInOut,
+    ));
+
+    _iconScaleAnimation = Tween<double>(
+      begin: 7,
+      end: 6,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    _containerScaleAnimation = Tween<double>(
+      begin: 2.0,
+      end: 0.4,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.bounceOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _controller
+      ..reset()
+      ..forward();
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3))
+            ]),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: 100,
+            minWidth: 100,
+            maxHeight: MediaQuery.of(context).size.height * 0.8,
+            maxWidth: MediaQuery.of(context).size.width * 0.8,
+          ),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 160,
+                    ),
+                    Text(
+                      widget.title,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      widget.subtitle,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned.fill(
+                  child: SlideTransition(
+                position: _yAnimation,
+                child: ScaleTransition(
+                  scale: _containerScaleAnimation,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green,
+                    ),
+                    child: ScaleTransition(
+                      scale: _iconScaleAnimation,
+                      child: widget.child,
+                    ),
+                  ),
+                ),
+              ))
+            ],
+          ),
         ),
       ),
     );
